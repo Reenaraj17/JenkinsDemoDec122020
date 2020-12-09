b32760df6ac64f274d5e732e68158b5d8772f17f
Reena.Raj <reenaraj17@gmail.com>
2020-12-09 23:11:34 +0000

    Committing the project to github first time

---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class TestApplication {
+    @Test
+    public void test1() {
+        System.out.println("Running in test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Running in test2");
+    }
+}
